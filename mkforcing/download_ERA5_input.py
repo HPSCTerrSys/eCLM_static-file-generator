22c26509c6b74621afb3ca51dbf79f675bec0188
Johannes Keller <jo.keller@fz-juelich.de>
2025-10-16 08:51:08 +0200

    add flexibility of `generate_datarequest`
    `dataset`, `request`, `target` as possible inputs

--- mkforcing/download_ERA5_input.py
+++ mkforcing/download_ERA5_input.py
@@ -42,50 +42,63 @@ def generate_days(year, month):
 
     return days
 
-def generate_datarequest(year, monthstr, days):
-    
+def generate_datarequest(year, monthstr, days,
+                        dataset="reanalysis-era5-single-levels",
+                        request=None,
+                        target=None):
+    """Generate and execute ERA5 data download request.
+
+    Args:
+        year (int): Year to download
+        monthstr (str): Month as zero-padded string (e.g., '07')
+        days (list): List of days in the month
+        dataset (str, optional): CDS dataset name. Defaults to 'reanalysis-era5-single-levels'.
+        request (dict, optional): Custom CDS request dictionary. If None, uses default request.
+        target (str, optional): Output filename. If None, uses 'download_era5_YYYY_MM.zip'.
+
+    Returns:
+        str: Path to downloaded file
+    """
+
     # active download client for climate data service (cds)
     client = cdsapi.Client()
-    
-    # dataset to download rom cds
-    dataset = "reanalysis-era5-single-levels"
-    # request for cds
-    request = {
-        "product_type": ["reanalysis"],
-        "variable": [
-            "surface_pressure",
-            "mean_surface_downward_long_wave_radiation_flux",
-            "mean_surface_downward_short_wave_radiation_flux",
-            "mean_total_precipitation_rate"
-        ],
-        "year": [str(year)],
-        "month": [monthstr],
-        "day": days,
-        "time": [
-            "00:00", "01:00", "02:00",
-            "03:00", "04:00", "05:00",
-            "06:00", "07:00", "08:00",
-            "09:00", "10:00", "11:00",
-            "12:00", "13:00", "14:00",
-            "15:00", "16:00", "17:00",
-            "18:00", "19:00", "20:00",
-            "21:00", "22:00", "23:00"
-        ],
-        "data_format": "netcdf",
-        "download_format": "unarchived",
-        "area": [74, -42, 20, 69]
-    }
-    # filename of downloaded file
-    target = 'download_era5_'+str(year)+'_'+monthstr+'.zip'
-    
+
+    # Default request if not provided
+    if request is None:
+        request = {
+            "product_type": ["reanalysis"],
+            "variable": [
+                "surface_pressure",
+                "mean_surface_downward_long_wave_radiation_flux",
+                "mean_surface_downward_short_wave_radiation_flux",
+                "mean_total_precipitation_rate"
+            ],
+            "year": [str(year)],
+            "month": [monthstr],
+            "day": days,
+            "time": [
+                "00:00", "01:00", "02:00",
+                "03:00", "04:00", "05:00",
+                "06:00", "07:00", "08:00",
+                "09:00", "10:00", "11:00",
+                "12:00", "13:00", "14:00",
+                "15:00", "16:00", "17:00",
+                "18:00", "19:00", "20:00",
+                "21:00", "22:00", "23:00"
+            ],
+            "data_format": "netcdf",
+            "download_format": "unarchived",
+            "area": [74, -42, 20, 69]
+        }
+
+    # Default filename if not provided
+    if target is None:
+        target = f'download_era5_{year}_{monthstr}.zip'
+
     # Get the data from cds
     client.retrieve(dataset, request, target)
 
-if __name__ == "__main__":
-    # Check if the correct number of arguments are provided
-    if len(sys.argv) != 4:
-        print("Usage: python download_ERA5_input.py <year> <month> <output_directory>")
-        sys.exit(1)
+    return target
 
     # Get the year and month from command-line arguments
     year = int(sys.argv[1])
@@ -105,5 +118,6 @@ if __name__ == "__main__":
     # Get the list of days for the request
     days = generate_days(year, month)
 
-    # do download request
-    generate_datarequest(year, monthstr, days)
+    # Execute download request
+    target = generate_datarequest(year, monthstr, days)
+    print(f"Download complete: {target}")
